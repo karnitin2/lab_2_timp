88405798693612db5cb20247f14cd82cd0429df0
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-01-25 14:16:41 +0300

    Update main.cpp

--- l_2.1/main.cpp
+++ l_2.1/main.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
-#include <cctype>
-#include "Cipher.h"
+#include "modAlphaCipher.h"
 using namespace std;
-void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+void check(const wstring& Text, const wstring& key, bool destructCipherText=false)
 {
+    wstring cipherText;
+    wstring decryptedText;
     try {
-        wstring cipherText;
-        wstring decryptedText;
-        Cipher cipher(key);
-        cipherText = cipher.encrypt(Text);
-        if (destructCipherText)
-            cipherText.front() = tolower(cipherText.front());
-        decryptedText = cipher.decrypt(cipherText);
-        wcout<<"key="<<key<<endl;
-        wcout<<Text<<endl;
-        wcout<<cipherText<<endl;
-        wcout<<decryptedText<<endl;
+         modAlphaCipher cipher(key);
+         cipherText = cipher.encrypt(Text);
+         decryptedText = cipher.decrypt(cipherText);
+         wcout<<L"key=" << key <<endl;
+         wcout<<Text<<endl;
+         wcout<<cipherText<<endl;
+         wcout<<decryptedText<<endl<<endl;
     } catch (const cipher_error & e) {
-        cerr<<"Error: "<<e.what()<<endl;
+ cerr << "Error: " << e.what() << endl;
     }
 }
 int main(int argc, char **argv)
 {
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"ПРИВЕТ");
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"");
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"П1Р2И3В4Е5Т6");
-    wcout<<L'-'<<endl;
-    check(L"ЭТО РАНДОМНЫЙ ТЕКСТ",L"ПРИВЕТ");
-    wcout<<L'-'<<endl;
-    check(L"ЭТОРАНДОМНЫЙТЕКСТ",L"ПРИВЕТ",true);
-    wcout<<L'-'<<endl;
-    check(L"21908321786912367801327860",L"ПРИВЕТ");
-    wcout<<L'-'<<endl;
-    return 0;
+     locale loc("ru_RU.UTF-8");
+     locale::global(loc);
+     check(L"АЛФАВИТ",L"АБВГД");
+     check(L"ЮНОСТЬ",L"ПИЖЕН55");
+     check(L"ДРУЖБА",L"");
+     check(L"0ПОЦЕЛЬСИЮ",L"РТИП");
+     return 0;
 }
