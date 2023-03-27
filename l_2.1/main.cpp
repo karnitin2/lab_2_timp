f547978443cf18257200f19d15658b6442dae7c7
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-03-27 19:20:09 +0300

    Update main.cpp

--- l_2.1/main.cpp
+++ l_2.1/main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
+#include <cctype>
 #include "modAlphaCipher.h"
 using namespace std;
-void check(const wstring& Text, const wstring& key, bool destructCipherText=false)
+void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
 {
-    wstring cipherText;
-    wstring decryptedText;
     try {
-         modAlphaCipher cipher(key);
-         cipherText = cipher.encrypt(Text);
-         decryptedText = cipher.decrypt(cipherText);
-         wcout<<L"key=" << key <<endl;
-         wcout<<Text<<endl;
-         wcout<<cipherText<<endl;
-         wcout<<decryptedText<<endl<<endl;
+        wstring cipherText;
+        wstring decryptedText;
+        modAlphaCipher cipher(key);
+        cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = tolower(cipherText.front());
+        decryptedText = cipher.decrypt(cipherText);
+        wcout<<"key="<<key<<endl;
+        wcout<<Text<<endl;
+        wcout<<cipherText<<endl;
+        wcout<<decryptedText<<endl;
     } catch (const cipher_error & e) {
- cerr << "Error: " << e.what() << endl;
+        cerr<<"Error: "<<e.what()<<endl;
     }
 }
 int main(int argc, char **argv)
 {
-     locale loc("ru_RU.UTF-8");
-     locale::global(loc);
-     check(L"АЛФАВИТ",L"АБВГД");
-     check(L"ЮНОСТЬ",L"ПИЖЕН55");
-     check(L"ДРУЖБА",L"");
-     check(L"0ПОЦЕЛЬСИЮ",L"РТИП");
-     return 0;
+    std::locale loc("ru_RU.UTF-8");
+    std::locale::global(loc);
+    check(L"Димаревинильясёмовалексейивашкинтут",L"Начало");
+    check(L"Димаревинильясёмовалексейивашкинтут",L"");
+    check(L"Димаревинильясёмовалексейивашкинтут",L"ДРУГ777");
+    check(L"Дима ревин илья сёмов алексей ивашкин тут",L"СЕРЕДИНА");
+    check(L"Димаревинильясёмовалексейивашкинтут",L"КОНЕЦ",true);
+    check(L"777",L"ДРУГ");
+    return 0;
 }
