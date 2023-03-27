4f4d72f0fa3a85afe4c757764e535447b0d4f30d
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-03-27 19:21:05 +0300

    Update modAlphaCipher.h

--- l_2.1/modAlphaCipher.h
+++ l_2.1/modAlphaCipher.h
@@ -1,31 +1,28 @@
-#pragma once
-#include <vector>
-#include <string>
-#include <map>
+#include <codecvt>
 class modAlphaCipher
 {
 private:
-     std::wstring numAlpha = L"АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ";//алфавит по порядку
-     std::map <wchar_t,int> alphaNum; //ассоциативный массив "номер по символу"
-     std::vector <int> key; //ключ
-     std::vector <int> convert(const std::wstring& s); //преобразование строка-вектор
-     std::wstring convert(const std::vector<int>& v); //преобразование вектор-строка
-     std::wstring getValidKey(const std::wstring & in); //Проверка правильный ли ключ задали
-     std::wstring getValidOpenText(const std::wstring & in); //Проверка на правильность открытого текста
-     std::wstring getValidCipherText(const std::wstring & in); //Проверка на правильность зашифрованного текста
- public:
-     modAlphaCipher()=delete; //запретим конструктор без параметров
-     modAlphaCipher(const std::wstring& skey); //конструктор для установки ключа
-     std::wstring encrypt(const std::wstring& open_text); //зашифрование
-     std::wstring decrypt(const std::wstring& cipher_text);//расшифрование
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> codec;
+    std::wstring numAlpha =L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    std::map <wchar_t,int> alphaNum;
+    std::vector <int> key;
+    std::vector<int> convert(const std::wstring& s);
+    std::wstring convert(const std::vector<int>& v);
+    std::wstring getValidKey(const std::wstring & s);
+    std::wstring getValidOpenText(const std::wstring & s);
+    std::wstring getValidCipherText(const std::wstring & s);
+public:
+    modAlphaCipher()=delete;
+    modAlphaCipher(const std::wstring& skey);
+    std::wstring encrypt(const std::wstring& open_text);
+    std::wstring decrypt(const std::wstring& cipher_text);
 };
+
 class cipher_error: public std::invalid_argument
 {
 public:
-     explicit cipher_error (const std::string& what_arg): //explicit запрещает компилятору приравнивать "Object a = 5" и "Object
-a(5)".
-//Делает доступным только второе
-         std::invalid_argument(what_arg) {}
-     explicit cipher_error (const char* what_arg):
-         std::invalid_argument(what_arg) {}
+    explicit cipher_error (const std::string& what_arg):
+        std::invalid_argument(what_arg) {}
+    explicit cipher_error (const char* what_arg):
+        std::invalid_argument(what_arg) {}
 };
