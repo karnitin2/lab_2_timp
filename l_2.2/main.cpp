a4cc01b5c977057220906be747ae265355f57ba7
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-03-27 19:29:54 +0300

    Update main.cpp

--- l_2.2/main.cpp
+++ l_2.2/main.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-#include <string>
-#include "Encryptor.h"
+#include <cctype>
+#include "modAlphaCipher.h"
+#include <locale>
 using namespace std;
-void check(const string& text1, const int& KEY)
+void check(const wstring& Text, const int & key)
 {
-     try {
-         Encryptor a;
-         string privat_text = a.encrypt(text1, KEY);
-         string decode_text = a.decrypt(privat_text, KEY);
-         cout << text1 << "\tIncoming text" << endl;
-         cout << privat_text << "\tEncrypted" <<endl;
-         cout << decode_text << "\tDeciphered" << endl;
-     } catch (const cipher_error& e) {
-         cerr << "\nError: " << e.what() << endl<< endl;
-     }
+    wstring t=Text;
+    try {
+        modAlphaCipher cipher(key);
+        wcout<<cipher.encrypt(key, t)<<endl;
+        wstring en=cipher.encrypt(key, t);
+        wcout<<cipher.decrypt(key, en)<<endl;
+    } catch (const cipher_error & e) {
+        cerr<<"Error: "<<e.what()<<endl;
+    }
 }
-int main(int argc, char **argv)
+int main()
 {
-     cout << "Correct:\n";
-     check("HELLOWORLD", 7);
-     cout << "\nIncorrect:\n";
-     check("",10);
-     cout << "\nIncorrect:\n";
-     check("HELLOWORLD1234",5);
-     cout << "\nIncorrect:\n";
-     check("HELLOWORLD1234",5);
-     cout << "\nIncorrect:\n";
-     check("HELLOWORLD1234",0);
-     return 0;
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    check(L"PLOHO",2);
+    check(L"PloHo",5);
+    check(L"PLOHO",0);
+    return 0;
 }
