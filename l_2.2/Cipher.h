3eb9fcc3f3cf226571b52d87f4bac8341e6470e9
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-05-25 23:04:56 +0300

    Update and rename modAlphaCipher.h to Cipher.h

---
+++ l_2.2/Cipher.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+class Cipher
+{
+public:
+    string cipher(string str, const int key);
+    string decipher(string str, const int key);
+};
+class cipher_error : public invalid_argument
+{
+public:
+	explicit cipher_error(const string& what_arg) :
+		invalid_argument(what_arg) {}
+	explicit cipher_error(const char* what_arg) :
+		invalid_argument(what_arg) {}
+};
