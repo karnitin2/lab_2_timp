cc5a242b6653a9ff3972e463879dc116b7701e84
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-05-25 23:04:12 +0300

    Update main.cpp

--- l_2.2/main.cpp
+++ l_2.2/main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
-#include <cctype>
-#include "modAlphaCipher.h"
-#include <locale>
+#include <string>
+#include "Cipher.h"
 using namespace std;
-void check(const wstring& Text, const int & key)
-{
-    wstring t=Text;
-    try {
-        modAlphaCipher cipher(key);
-        wcout<<cipher.encrypt(key, t)<<endl;
-        wstring en=cipher.encrypt(key, t);
-        wcout<<cipher.decrypt(key, en)<<endl;
-    } catch (const cipher_error & e) {
-        cerr<<"Error: "<<e.what()<<endl;
-    }
-}
+string cipher(string str, const int key);
+string decipher(string str, const int key);
 int main()
 {
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    check(L"PLOHO",2);
-    check(L"PloHo",5);
-    check(L"PLOHO",0);
+    Cipher cpr;
+    int number = 7;
+    try {
+        string original = "NACHALOVOINI";
+        string encrypted = cpr.cipher(original, number);
+        string decrypted = cpr.decipher(encrypted, number);
+        cout << original << endl;
+        cout << encrypted << endl;
+        cout << decrypted << endl;
+    } catch (const cipher_error & e) {
+        cerr << "Error: " << e.what() << endl;
+    }
     return 0;
 }
