2b9090b739791df0e3a872e4dd0af489108f723d
karnitin2 <112693502+karnitin2@users.noreply.github.com>
2023-05-26 13:51:01 +0300

    Update Cipher.h

--- l_2.2/Cipher.h
+++ l_2.2/Cipher.h
@@ -6,14 +6,15 @@ using namespace std;
 class Cipher
 {
 public:
+    int key ();
     string cipher(string str, const int key);
     string decipher(string str, const int key);
 };
 class cipher_error : public invalid_argument
 {
 public:
-	explicit cipher_error(const string& what_arg) :
-		invalid_argument(what_arg) {}
-	explicit cipher_error(const char* what_arg) :
-		invalid_argument(what_arg) {}
+    explicit cipher_error(const string& what_arg) :
+        invalid_argument(what_arg) {}
+    explicit cipher_error(const char* what_arg) :
+        invalid_argument(what_arg) {}
 };
